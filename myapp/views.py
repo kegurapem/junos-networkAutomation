89c2aa6a5bcd6441d2baab95350e0f7007c31afe
kevin gutierrez <harold.dev1993@gmail.com>
2023-08-02 00:17:19 -0500

    importacion de una funcion y mostrarlo en la interfaz OK

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from nornir.app import hello
+
+# data = hello()
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'hello': hello()
+    })
