e68bfdbe783783902cc8601e5ed74c3e75080776
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2023-08-04 19:14:25 -0500

    avanzando ando

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from nornir.app import hello
 
 # data = hello()
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {
-        'hello': hello()
-    })
+# def index(request):
+#     return render(request, 'index.html', {
+#         'hello': hello()
+#     })
+# ***********************************************************************************
+
+def mi_vista(request):
+    if request.method == 'POST':
+        switch1 = request.POST.get('switch1')
+        switch2 = request.POST.get('switch2')  
+
+        print(type(switch1))   
+        print(switch2)
+
+        if switch1 == 'True':
+            def hello():
+                return  print('Hello World')
+
+        return render(request, 'resultado.html', {'switch1': switch1, 'switch2': switch2})
+
+    return render(request, 'index2.html')
