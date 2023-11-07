a68d254784ddbdeeb29f34cc0c79f731d9348d4e
Kevin Harold Gutierrez Ramirez <harold.dev1993@gmail.com>
2023-11-07 18:39:49 -0500

    mejorando la interfaz

--- proyectnornir/tasks/main.py
+++ proyectnornir/tasks/main.py
@@ -29,25 +29,6 @@ def serialize_results_to_json(config_file="/home/kevin/junos-networkAutomation/p
         archivo.write(data_json)
 
 
-# def create_user(name_user, password_user):
-#     # Inicializa Nornir
-#     nr = InitNornir(config_file="/home/kevin/junos-networkAutomation/proyectnornir/config.yaml")
-
-#     # Comandos para crear el usuario y configurar el dispositivo
-#     commands = [
-#         f"set system login user {name_user} uid 2020 class super-user",
-#         f'set system login user {name_user} full-name "usuario administrador"',
-#         f"set system login user {name_user} authentication plain-text-password-value {password_user}",
-#         "commit",
-#     ]
-
-#     # Ejecuta la tarea en el dispositivo
-#     result = nr.run(task=netmiko_send_config, config_commands=commands)
-
-#     # Puedes verificar la salida si lo deseas
-#     print(result)
-
-
 def create_user(name_user, password_user, permiso):
     # Inicializa Nornir
     nr = InitNornir(config_file="/home/kevin/junos-networkAutomation/proyectnornir/config.yaml")
---
+++ prueba/get_facts.py
@@ -0,0 +1,16 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_get
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+
+results = nr.run(
+    task=napalm_get, getters=["get_facts"]
+)
+
+print_result(results)
+
+
